030fe63642cd4bfdbb2a1f430f1b98f961fac806
Shaoqi <shaoqigogo@gmail.com>
2023-07-30 22:23:30 +0200

    Porous UP works?

--- tests/main_test_biot_equation.py
+++ tests/main_test_biot_equation.py
@@ -34,7 +34,7 @@ from fem.analytical_sol import DoubleleLayerKundltTube
 
 
 def test_case():
-    num_elem = 10  # number of elements
+    num_elem = 100  # number of elements
     num_nodes = num_elem + 1  # number of nodes
 
     nodes = np.linspace(0, 1, num_nodes)
@@ -72,7 +72,6 @@ def test_case():
     print("bases global dofs index: ", dof_handler.base4global_dofs())
     print("global dofs index for P: ", dof_handler.get_global_dofs_by_base('P'))
     print("global dofs index for Ux: ", dof_handler.get_global_dofs_by_base('Ux'))
-    import pdb; pdb.set_trace()
 
     # print(dof_handler.num_internal_dofs)
     # print(dof_handler.num_external_dofs)
@@ -85,7 +84,11 @@ def test_case():
     alpha        = 1.17  # Tortuosity
     Lambda_prime = 742e-6  # Viscous characteristic length
     Lambda      = 110e-6  # 
-    xfm = EquivalentFluid('xfm', phi, sigma, alpha, Lambda_prime, Lambda)
+    rho_1 = 22.1
+    nu = 0.39
+    E = 70e3
+    eta = 0.265
+    xfm = PoroElasticMaterial('xfm', phi, sigma, alpha, Lambda_prime, Lambda, rho_1, E, nu, eta)
 
     # Harmonic Acoustic problem define the frequency
     freq = 2000
@@ -98,54 +101,56 @@ def test_case():
 
 
     # initialize the assembler
-    P_assembler = Assembler4Biot(dof_handler, P_bases, subdomains, dtype=np.complex128)
+    assembler = Assembler4Biot(dof_handler, subdomains, dtype=np.complex128)
 
-    K_g= assembler.assemble_material_K(omega)  # global stiffness matrix with material attribution
-    M_g= assembler.assemble_material_M(omega)  # global mass matrix with material attribution
+    K_p= assembler.assemble_material_K(P_bases, 'P', omega)  # global stiffness matrix with material attribution
+    M_p= assembler.assemble_material_M(P_bases, 'P', omega)  # global mass matrix with material attribution
     # print("K_g:", assembler.get_matrix_in_array(K_g))
     # print("M_g:", assembler.get_matrix_in_array(M_g))
 
+    K_u= assembler.assemble_material_K(Ux_bases, 'Ux', omega)  # global stiffness matrix with material attribution
+    M_u= assembler.assemble_material_M(Ux_bases, 'Ux', omega)  # global mass matrix with material attribution
+
+    C_pu= assembler.assemble_material_C(P_bases, 'P', 'Ux', omega)  # global coupling matrix with material attribution
+    C_up= C_pu.T  # global coupling matrix with material attribution 
     # construct linear system
     # K_u-M_u - C+(K_p-M_p -C.T)
-    left_hand_matrix = K_g-M_g
-    # plot_matrix_partten(left_hand_matrix)
+    left_hand_matrix = K_p+K_u-M_u-M_p - C_pu-C_up
+    plot_matrix_partten(left_hand_matrix)
 
+    essential_bcs = {'type': 'solid_displacement', 'value': 0, 'position': num_elem}
+    assembler.apply_essential_bc(left_hand_matrix, essential_bcs, var='Ux',)
     # print(assembler.get_matrix_in_array(left_hand_matrix))
     #  natural boundary condition   
-    nature_bcs = {'type': 'velocity', 'value': 1*np.exp(-1j*omega), 'position': 0}
-    right_hand_vector = assembler.assemble_nature_bc(nature_bcs)
+    nature_bcs = {'type': 'total_displacement', 'value': 1*np.exp(-1j*omega), 'position': 0}
+    right_hand_vector = assembler.apply_nature_bc(nature_bcs)
     # print(right_hand_vector)
 
     # solver the linear system
     linear_solver = LinearSolver(dof_handler)
-    # left_hand_matrix, right_hand_vector = linear_solver.optimize_matrix_pattern(left_hand_matrix, right_hand_vector)
-    # left_hand_matrix = left_hand_matrix[optimal_order, optimal_order]
-    # print(left_hand_matrix[0], left_hand_matrix[1])
     # plot_matrix_partten(left_hand_matrix)
     linear_solver.solve(left_hand_matrix, right_hand_vector)
     sol = linear_solver.u
-    # print("solution:", abs(sol))
 
 
     # ====================== Analytical Solution ======================
     # analytical solution
-    kundlt_tube = DoubleleLayerKundltTube(mesh, air, xfm, omega, nature_bcs)
-    ana_sol = np.zeros(num_nodes, dtype=np.complex128)  #initialize the analytical solution vector
-    kundlt_tube.sol_on_nodes(ana_sol, sol_type='pressure')
 
+    import pdb; pdb.set_trace()
     # plot the solution
     post_processer = PostProcessField(mesh.nodes, r'1D Helmholtz (2000$Hz$)')
-    post_processer.plot_sol((np.real(sol), f'FEM ($p=3$)', 'solid'), (np.real(ana_sol), 'Analytical', 'dashed'))
+    # post_processer.plot_sol((np.real(sol), f'FEM ($p=3$)', 'solid'), (np.real(ana_sol), 'Analytical', 'dashed'))
+    post_processer.plot_sol((np.real(sol[:101]), f'FEM ($p=3$)', 'solid'))
     plt.show()
 
-    error = post_processer.compute_error(sol, ana_sol)
-    print("error:", error)
-    if error < 1e-4:
-        print("Test passed!")
-        return True
-    else:
-        print("Test failed!")
-        return False
+    # error = post_processer.compute_error(sol, ana_sol)
+    # print("error:", error)
+    # if error < 1e-4:
+    #     print("Test passed!")
+    #     return True
+    # else:
+    #     print("Test failed!")
+    #     return False
 
 if __name__ == "__main__":
     result = test_case()
